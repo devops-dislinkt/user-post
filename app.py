fafae0ebdc3167685d1b754993d9e3096dd9bb28
Mitar Perovic <perovicmitar@uns.ac.rs>
2022-06-10 17:47:03 +0200

    feat:tracing with opentelemetry

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from app import create_app
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
 if __name__ == '__main__':
     app = create_app()
+    FlaskInstrumentor().instrument_app(app)
     app.run(debug=True, host='0.0.0.0', port=8080)
 
 #port = int(os.environ.get('PORT', 8080))
