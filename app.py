88e4132e50154244fd1794db6494e6eff6647797
Mitar Perovic <perovicmitar@uns.ac.rs>
2022-06-11 20:45:51 +0200

    added jaeger tracing

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 from app import create_app
-from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
 
 if __name__ == '__main__':
     app = create_app()
-    FlaskInstrumentor().instrument_app(app)
+
+    config = Config(
+        config={
+            'sampler':{
+                'type': 'const',
+                'param': 1,
+            },
+            'local_agent':{
+                'reporting_host': 'jaeger',  # ili u ENV dodati IP adresu kontejnera
+                'reporting_port': '6831'
+            },
+            'logging': True
+        }
+    )
+
+    jaeger_tracer = config.initialize_tracer()
+    tracing = FlaskTracing(jaeger_tracer, True, app)
+
     app.run(debug=True, host='0.0.0.0', port=8080)
 
 #port = int(os.environ.get('PORT', 8080))
